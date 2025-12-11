3d54dbe5825cfdd248e5796e5b3c835edaad0828
Rakesh Nagapuri <Rakesh.Nagapuri@planonsoftware.com>
2025-12-11 20:14:49 +0530

    add base package structure for PFM

---
+++ src/main/java/com/pfm/backend/PfmBackendApplication.java
@@ -0,0 +1,13 @@
+package com.pfm.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PfmBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PfmBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pfm/backend/PfmBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pfm.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PfmBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
